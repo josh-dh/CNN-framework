87a16fbfa553f8437e6656b1c0c3859650f10f05
josh-dh <jdonellyhiggins@gmail.com>
2017-08-11 12:08:11 -0700

    separated filters from conv_layer

--- framework.py
+++ framework.py
@@ -4,10 +4,11 @@ import numpy as np
 
 #input shape: (rows, columns, color: 3 if rgb, 1 if greyscale)
 
-def conv_layer(inputimage, zero_pad_dimensions, filter_count, filter_dimensions, train=False, init=False):
-	#initialize filters with gaussian noise
-	def init_filters(filter_count, filter_dimensions):
-		return np.array([np.random.normal(scale=50,size=filter_dimensions) for i in range(filter_count)])
+#initialize filters with gaussian noise
+def init_filters(filter_count, filter_dimensions):
+	return np.array([np.random.normal(scale=50,size=filter_dimensions) for i in range(filter_count)])
+
+def conv_layer(inputimage, zero_pad_dimensions, filters, train=False):
 	#zero pad equally on x and y axis equally per axis
 	def zero_pad(inputimage, zero_pad_dimensions):
 		if len(inputimage.shape) == 3:
