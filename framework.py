0bde379bbdbeadd747e550fa7a52eb33bb67fe9c
josh-dh <jdonellyhiggins@gmail.com>
2017-08-11 12:04:15 -0700

    init_filters and zero_pad fixed

--- framework.py
+++ framework.py
@@ -4,16 +4,27 @@ import numpy as np
 
 #input shape: (rows, columns, color: 3 if rgb, 1 if greyscale)
 
-def conv_layer(input, filter_count, filter_dimensions, zero_pad_dimensions, train=false, init=false):
+def conv_layer(inputimage, zero_pad_dimensions, filter_count, filter_dimensions, train=False, init=False):
 	#initialize filters with gaussian noise
 	def init_filters(filter_count, filter_dimensions):
-		filters = np.array([])
-		for i in range(filter_count):
-			np.append(filter, np.random.normal(scale=50,size=filter_dimensions), axis=0)
-		return filters
+		return np.array([np.random.normal(scale=50,size=filter_dimensions) for i in range(filter_count)])
 	#zero pad equally on x and y axis equally per axis
-	def zero_pad(input, zero_pad_dimensions):
-		return np.append(
-			np.zeros((input.shape()[0],zero_pad_dimensions[1])),
-			np.append(np.zeros((zero_pad_dimensions[0],(input.shape()[1]))),input,np.zeros((zero_pad_dimensions[0],(input.shape()[1]))), axis=1),
-			np.zeros((input.shape()[0],zero_pad_dimensions[1]))
+	def zero_pad(inputimage, zero_pad_dimensions):
+		if len(inputimage.shape) == 3:
+			inner = np.concatenate(
+					(np.zeros((zero_pad_dimensions[0],inputimage.shape[1],inputimage.shape[2])),
+					inputimage,
+					np.zeros((zero_pad_dimensions[0],inputimage.shape[1], inputimage.shape[2]))), axis=1)
+			return np.concatenate(
+				(np.zeros((zero_pad_dimensions[1],inner.shape[1], inner.shape[2])),
+				inner,
+				np.zeros((zero_pad_dimensions[1],inner.shape[1], inner.shape[2]))), axis=0)
+		else:
+			inner = np.concatenate(
+					(np.zeros((zero_pad_dimensions[0],inputimage.shape[1])),
+					inputimage,
+					np.zeros((zero_pad_dimensions[0],inputimage.shape[1]))), axis=1)
+			return np.concatenate(
+				(np.zeros((zero_pad_dimensions[1],inner.shape[1])),
+				inner,
+				np.zeros((zero_pad_dimensions[1],inner.shape[1]))), axis=0)
