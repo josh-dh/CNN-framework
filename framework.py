064f234e1c7d4a4f2436518137ad3c975c617951
josh-dh <jdonellyhiggins@gmail.com>
2017-08-12 14:06:02 -0700

    added fully connected layer, softmax, and relu activations

--- framework.py
+++ framework.py
@@ -5,47 +5,54 @@ import numpy as np
 #input shape: (rows, columns, color: 3 if rgb, 1 if greyscale)
 
 #initialize filters with gaussian noise
-def init_filters(filter_count, filter_dimensions):
+def init_weights(filter_count, filter_dimensions):
 	return np.array([np.random.normal(scale=5,size=filter_dimensions) for i in range(filter_count)])
 
 #initialize
 def init_biases(bias_count):
-	return np.random.normal(scale=100,size=bias_count)
+	return np.random.normal(scale=5,size=bias_count)
 
 #convolutional layer
-def conv_layer(inputimage, filters, biases, zero_pad_dimensions=(0,0), stride=(1,1), train=False):
+def conv_layer(inputobject, filters, biases, zero_pad_dimensions=(0,0), stride=(1,1), train=False):
 	#zero pad equally on x and y axis equally per axis
-	def zero_pad(inputimage, zero_pad_dimensions):
-		if len(inputimage.shape) > 2: #multidemsional
+	def zero_pad(inputobject, zero_pad_dimensions):
+		if len(inputobject.shape) > 2: #multidemsional
 			inner = np.concatenate(
-					(np.zeros((zero_pad_dimensions[0],inputimage.shape[1],inputimage.shape[2])),
-					inputimage,
-					np.zeros((zero_pad_dimensions[0],inputimage.shape[1], inputimage.shape[2]))), axis=1)
+					(np.zeros((zero_pad_dimensions[0],inputobject.shape[1],inputobject.shape[2])),
+					inputobject,
+					np.zeros((zero_pad_dimensions[0],inputobject.shape[1], inputobject.shape[2]))), axis=1)
 			return np.concatenate(
 				(np.zeros((zero_pad_dimensions[1],inner.shape[1], inner.shape[2])),
 				inner,
 				np.zeros((zero_pad_dimensions[1],inner.shape[1], inner.shape[2]))), axis=0)
 		else: #uni-dimensional
 			inner = np.concatenate(
-					(np.zeros((zero_pad_dimensions[0],inputimage.shape[1])),
-					inputimage,
-					np.zeros((zero_pad_dimensions[0],inputimage.shape[1]))), axis=1)
+					(np.zeros((zero_pad_dimensions[0],inputobject.shape[1])),
+					inputobject,
+					np.zeros((zero_pad_dimensions[0],inputobject.shape[1]))), axis=1)
 			return np.concatenate(
 				(np.zeros((zero_pad_dimensions[1],inner.shape[1])),
 				inner,
 				np.zeros((zero_pad_dimensions[1],inner.shape[1]))), axis=0)
 	#convolute filters across image and return result
-	def convolute(inputimage, filters, biases, stride):
-		output = np.zeros(((inputimage.shape[0]-filters.shape[1])/stride[0],(inputimage.shape[1]-filters.shape[2])/stride[1], filters.shape[0]))
+	def convolute(inputobject, filters, biases, stride):
+		output = np.zeros(((inputobject.shape[0]-filters.shape[1])/stride[0],(inputobject.shape[1]-filters.shape[2])/stride[1], filters.shape[0]))
 		for i in range(filters.shape[0]):
-			for j in range((inputimage.shape[0]-filters.shape[1])/stride[0]): #rows
-				for k in range((inputimage.shape[1]-filters.shape[2])/stride[1]): #columns
+			for j in range((inputobject.shape[0]-filters.shape[1])/stride[0]): #rows
+				for k in range((inputobject.shape[1]-filters.shape[2])/stride[1]): #columns
 					output[j,k,i] = (biases[i] + np.vdot(
-						inputimage[np.ix_(np.arange(j*stride[0], j*stride[0] + filters.shape[1]), np.arange(k*stride[1], k*stride[1] + filters.shape[2]))],
+						inputobject[np.ix_(np.arange(j*stride[0], j*stride[0] + filters.shape[1]), np.arange(k*stride[1], k*stride[1] + filters.shape[2]))],
 						filters[i]))
 		return output
 
-	return convolute(zero_pad(inputimage, zero_pad_dimensions), filters, biases, stride)
+	return convolute(zero_pad(inputobject, zero_pad_dimensions), filters, biases, stride)
+
+#fully-connected layer
+def fulcon_layer(inputobject, weights, biases):
+	output = numpy.zeros((weights,))
+	for i in output:
+		output[i] = numpy.dot(inputobject.flatten(), weights[i]) + biases[i]
+	return output
 
 #cross entropy loss designed to work with one-hot labels and softmax predictions UNTESTED
 def loss(predictions, labels):
@@ -54,8 +61,16 @@ def loss(predictions, labels):
 		output += labels[i] * 1/np.log(predictions[i])
 	return output
 
-#returns the softmax of a 1d array UNTESTED
+#returns the softmax of a 1d array
 def softmax(array):
-	return np.divide(array, np.sum(exp(array)))
+	return np.divide(np.exp(array), np.sum(np.exp(array)))
 
-print(conv_layer(np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]]), init_filters(5, (2,2,3)), init_biases(5), zero_pad_dimensions=(2,2)))
+#relu activation function
+def relu(array):
+	array[array < 0] = 0
+	return array
+
+#stochastic gradient descent for loss minimization
+def sgd(lossfunc, batch):
+
+#print(relu(conv_layer(np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]]), init_weights(5, (2,2,3)), init_biases(5), zero_pad_dimensions=(2,2))))
