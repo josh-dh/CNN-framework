c42393648f1db62dfef32a37b918adc30ef1c614
josh-dh <jdonellyhiggins@gmail.com>
2017-08-19 13:24:32 -0700

    added quadratic loss function to work with one-hot layers [untested]; moved loss functions to new backprop section; added framework for final layer error function and recursive lower layer error function

--- framework.py
+++ framework.py
@@ -54,13 +54,6 @@ def fulcon_layer(inputobject, weights, biases):
 		output[i] = numpy.dot(inputobject.flatten(), weights[i]) + biases[i]
 	return output
 
-#cross entropy loss designed to work with one-hot labels and softmax predictions UNTESTED
-def loss(predictions, labels):
-	output = 0
-	for i in range(len(predictions)):
-		output += labels[i] * 1/np.log(predictions[i])
-	return output
-
 #returns the softmax of a 1d array
 def softmax(array):
 	return np.divide(np.exp(array), np.sum(np.exp(array)))
@@ -70,7 +63,25 @@ def relu(array):
 	array[array < 0] = 0
 	return array
 
-#stochastic gradient descent for loss minimization
-def sgd(lossfunc, batch):
+
+#BACKPROP
+
+#calculate the error of the final layer
+def final_layer_error():
+	def d_loss_d_activation(predictions, labels):
+
+#calculate the error of the layer lower to the last calculated layer
+def lower_layer_error():
+
+#cross entropy loss designed to work with one-hot labels and softmax predictions UNTESTED
+def losscrossentropy(predictions, labels):
+	output = 0
+	for i in range(len(predictions)):
+		output -= labels[i] * np.log(predictions[i])
+	return output/i
+
+#quadratic loss function for one-hot labels and softmax predictions
+def lossquadratic(predictions, labels):
+	return np.sum(np.subtract(labels, predictions))/(2^labels.size)
 
 #print(relu(conv_layer(np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]]), init_weights(5, (2,2,3)), init_biases(5), zero_pad_dimensions=(2,2))))
