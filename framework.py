264fe8299225e77faf8452c0ff146c97ef4b0b7f
josh-dh <jdonellyhiggins@gmail.com>
2017-09-29 13:28:10 -0400

    pre-compiled framework and verified that the entrire package is functional

--- framework.py
+++ framework.py
@@ -4,14 +4,7 @@ Joshua Donelly-Higgins
 
 #TODO: weight, bias gradients; test backprop
 
-import numpy as np
-import os
-from PIL import Image
-
 import backprop
 import import_files
 import layer
 import tests
-
-tests.test_full_net()
-tests.old_tests()
